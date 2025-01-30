6f310ad0eb58895a42f4fa5c1fd1d6798d394349
Anson Jia <ansonjia123@gmail.com>
2025-01-30 13:32:26 -0500

    added menu functions and updated styling

--- Stopwatch/index.js
+++ Stopwatch/index.js
@@ -44,3 +44,39 @@ function update(){
 
     display.textContent = `${hours}:${minutes}:${seconds}:${miliseconds}`;
 }
+
+
+
+
+
+function toClock() {
+    window.location.href = 'https://ansonjia.github.io/Clock-Stopwatch-Timer/';
+}
+
+function toStopwatch() {
+    window.location.href = 'https://ansonjia.github.io/Clock-Stopwatch-Timer/Stopwatch/';
+}
+
+function openNav() {
+    document.getElementById("iconBar").style.width = "90px";
+    document.getElementById("iconBar").style.border=  "0.5px solid white"; 
+    document.getElementById("open").style.position = "absolute"; 
+    document.getElementById("open").style.left = "100px";
+  }
+
+  function closeNav() {
+    document.getElementById("iconBar").style.width = "0";
+    document.getElementById("iconBar").style.border = "none";
+    document.getElementById("open").style.display="block"; 
+    document.getElementById("open").style.left = "10px";
+  }
+
+  let isNavOpen = false; 
+  function toggleNav() {
+    if (isNavOpen) {
+        closeNav();
+    } else {
+        openNav();
+    }
+    isNavOpen = !isNavOpen; 
+}
